049268e677af1c894a0933408356ab1c5d9cce55
soulk242 <69799332+soulk242@users.noreply.github.com>
2020-09-29 22:47:51 -0400

    Update item.jsx

--- item.jsx
+++ item.jsx
@@ -8,7 +8,7 @@ export default class Item extends Component {
     render() {
         return (
             <div className="alert alert-warning alert-dismissible" role="alert" style={this.alertSize}>
-            <button type="button" className="close" data-dismiss="alert" aria-label="Close">
+            <button type="button" className="close" data-dismiss="alert" aria-label="Close" onClick={() => this.props.onDelete(this.props.id)}>
                 <span className="glyphicon glyphicon-trash">&times;</span>
             </button>
             <strong>Warning!</strong> Better check yourself, you're not looking too good.
